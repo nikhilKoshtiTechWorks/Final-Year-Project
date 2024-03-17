e0abfbb98fd19ec322687ad161b60f9ed76f015d
Nikhil-Koshti <nikhilkoshti.techworks@gmail.com>
2024-03-17 20:10:45 +0530

    Changed the css
    Mainly, added new element called section-title.
    Moreover changed the color combination (added light blue)
    
    For biceps.html , managed to crop the image without streching it (object-fit: cover) and gave the desired dimentions.
    
    Next tasks: (for bicesps.html)
    1. Add camera permission
    2. Add loading element
    3. Sort out the instructions

--- app/app/static/js/script.js
+++ app/app/static/js/script.js
@@ -2,3 +2,8 @@ function redirectToPage() {
     // Replace 'example.html' with the URL you want to redirect to
     window.location.href = '/video_feed';
 }
+
+function redirectToHomePage() {
+    // Replace 'example.html' with the URL you want to redirect to
+    window.location.href = '/';
+}
